f55b3da2f607af480cc3dfc3bb23301f31544f2f
trl <dylanzhou415@gmail.com>
2025-10-06 00:28:28 +0200

    feat: add the model

---
+++ model.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32 # Number of heads for the queries
+    n_kv_heads: Optional[int] = None # Number of heads for the K and V
+    vocab_size: int = -1 # This will be set when we load the tokenizer
+    multiple_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    # Needed for KV Cache
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+    device: str = None
+
+ 
+S
+class Transformer(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.args = args
+        self.vocab_size = args.vocab_size
+        self.n_layers = args.n_layers
+        self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
+
+        self.layers = nn.ModuleList()
+
+        for _ in range(args.n_layers):
+            self.layers.append(EncoderBlock(args))
+        
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, self.vocab_size, bias=False)
+        
+        self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, 
+                                                              self.args.max_seq_len * 2, 
+                                                              device=self.args.device)
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        # tokens: (B, Seq_Len)
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token aS a time can be processed"
+
+        # (B, Seq_Len) -> (B, Seq_Len, Dim)
+        h = self.tok_embeddings(tokens)
+
+        # Retrieve the pairs(m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
+        freqs_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+
+        # Consecutively apply all the encoder layers
+        for layer in self.layers:
+            h = layer(h, freqs_complex)
+        h = self.norm(h)
+        output = self.output(h).float()
+        return output  # (B, Seq_Len, Vocab_Size)
+    
+
